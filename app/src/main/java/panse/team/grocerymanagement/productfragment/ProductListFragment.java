675cd1108d8b23c58dbc60fa090c4df71b9dd7d8
NguyenTanLinh <nguyentanlinh1707@gmail.com>
2018-11-11 10:08:22 +0700

    xoa product

--- app/src/main/java/panse/team/grocerymanagement/productfragment/ProductListFragment.java
+++ app/src/main/java/panse/team/grocerymanagement/productfragment/ProductListFragment.java
@@ -1,12 +1,15 @@
 package panse.team.grocerymanagement.productfragment;
 
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+
 import android.support.v4.app.ListFragment;
+import android.support.v7.app.AlertDialog;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -18,24 +21,25 @@ import java.util.Collections;
 
 import panse.team.grocerymanagement.FrameFuction;
 import panse.team.grocerymanagement.R;
-import panse.team.grocerymanagement.entities.Order;
+
 import panse.team.grocerymanagement.entities.Product;
 
-public class ProductListFragment extends ListFragment implements View.OnClickListener,FrameFuction {
-private ArrayList<Product> products;
-private ListView list;
-private ProductListAdapter adapter;
-private TextView tvProductIdHeader, tvProductNameHeader, tvProductQtyHeader, tvProductPriceHeader,tvProductInforHeader;
+public class ProductListFragment extends ListFragment implements View.OnClickListener, FrameFuction {
+    private ArrayList<Product> products;
+    private ListView list;
+    private ProductListAdapter adapter;
+    private TextView tvProductIdHeader, tvProductNameHeader, tvProductQtyHeader, tvProductPriceHeader, tvProductInforHeader;
 
 
     public void init(View view) {
         list = view.findViewById(android.R.id.list);
-      tvProductIdHeader = view.findViewById(R.id.tvProductIdHeader);
+        tvProductIdHeader = view.findViewById(R.id.tvProductIdHeader);
         tvProductNameHeader = view.findViewById(R.id.tvProductNameHeader);
         tvProductQtyHeader = view.findViewById(R.id.tvProductQtyHeader);
         tvProductPriceHeader = view.findViewById(R.id.tvProductPriceHeader);
-        tvProductInforHeader=view.findViewById(R.id.tvProductInforHeader);
+        tvProductInforHeader = view.findViewById(R.id.tvProductInforHeader);
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -44,28 +48,28 @@ private TextView tvProductIdHeader, tvProductNameHeader, tvProductQtyHeader, tvP
         registerEvent();
         registerForContextMenu(list);
         products = new ArrayList<Product>();
-products.add(new Product("12345678","nuoc suoi",8,80000.5,"nuoc uong"));
-        products.add(new Product("12345679","coca",5,50000,"nuoc uong"));
-        products.add(new Product("123456789","pesi",3,30000,"nuoc uong"));
-        products.add(new Product("123456","o long",10,100000,"nuoc uong"));
-Collections.sort(products,Product.ASC_productId);
-tvProductIdHeader.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.sharp_arrow_drop_down_24,0);
-        adapter = new ProductListAdapter(getActivity(),R.layout.custom_product_listview,products);
-setListAdapter(adapter);
+        products.add(new Product("12345678", "nuoc suoi", 8, 80000.5, "nuoc uong"));
+        products.add(new Product("12345679", "coca", 5, 50000, "nuoc uong"));
+        products.add(new Product("123456789", "pesi", 3, 30000, "nuoc uong"));
+        products.add(new Product("123456", "o long", 10, 100000, "nuoc uong"));
+        Collections.sort(products, Product.ASC_productId);
+        tvProductIdHeader.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.sharp_arrow_drop_down_24, 0);
+        adapter = new ProductListAdapter(getActivity(), R.layout.custom_product_listview, products);
+        setListAdapter(adapter);
         return view;
     }
 
-    public  void registerEvent() {
+    public void registerEvent() {
         tvProductIdHeader.setOnClickListener(this);
         tvProductNameHeader.setOnClickListener(this);
         tvProductQtyHeader.setOnClickListener(this);
         tvProductPriceHeader.setOnClickListener(this);
-      tvProductInforHeader.setOnClickListener(this);
+        tvProductInforHeader.setOnClickListener(this);
     }
 
 
     //biến quản lý sort
-            int click = 0;
+    int click = 0;
     String term = "";
 
     // Các sự kiện OnClick
@@ -77,18 +81,19 @@ setListAdapter(adapter);
                 sortProductId();
                 break;
             case R.id.tvProductNameHeader:
-                 sortProductName();
+                sortProductName();
                 break;
             case R.id.tvProductQtyHeader:
-                 sortProductQty();
+                sortProductQty();
                 break;
             case R.id.tvProductPriceHeader:
-                 sortProductPrice();
+                sortProductPrice();
                 break;
             // ^^^
 
         }
     }
+
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         if (v.getId() == android.R.id.list) {
@@ -97,10 +102,35 @@ setListAdapter(adapter);
             Product product = products.get(info.position);
 
             //setTitle cho menu
-            menu.setHeaderTitle( product.getProductName());
+            menu.setHeaderTitle(product.getProductName());
             inflater.inflate(R.menu.product_list_context_menu, menu);
         }
     }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        final AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.delete:
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Xóa sản phẩm");
+                builder.setMessage("Bạn có muốn xóa sản phẩm ?");
+                builder.setNegativeButton("Không", null);
+                builder.setPositiveButton("Có", new AlertDialog.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        products.remove(menuInfo.position);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+                builder.show();
+                break;
+
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
     //sap sep theo ma san pham
     public void sortProductId() {
         if (click == 0 && term.equals("")) {
@@ -115,7 +145,7 @@ setListAdapter(adapter);
             term = "";
             Collections.sort(products, Product.DES_productId);
             adapter.notifyDataSetChanged();
-           // resetArrow();
+            // resetArrow();
             tvProductIdHeader.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.sharp_arrow_drop_up_24, 0);
         }
     }
@@ -138,6 +168,7 @@ setListAdapter(adapter);
             tvProductNameHeader.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.sharp_arrow_drop_up_24, 0);
         }
     }
+
     //sap sep theo so luong
     public void sortProductQty() {
         if (click == 0 && term.equals("")) {
@@ -156,6 +187,7 @@ setListAdapter(adapter);
             tvProductQtyHeader.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.sharp_arrow_drop_up_24, 0);
         }
     }
+
     //sap sep theo gia
     public void sortProductPrice() {
         if (click == 0 && term.equals("")) {
